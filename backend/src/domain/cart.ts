633eeca6b89e8659487449d8eb47cea386ab0f2d
c-capocasali <caio.capocasali@usp.br>
2025-06-20 01:28:49 -0300

    definição de tipos do carrinho

--- backend/src/domain/cart.ts
+++ backend/src/domain/cart.ts
@@ -1,12 +1,32 @@
+import { z } from "zod";
+
 export type CartItem = {
-    productId: string;
-    name: string;
-    price: number;
-    discount: number;
-    image: string;
+    product: {
+        id: string;
+        name: string;
+        price: number;
+        discount: number;
+        image: string;
+    };
     quantity: number;
 }
 
 export type Cart = {
-    
+    id: string;
+    user: string;
+    items: CartItem[];
 }
+
+export const AddToCartSchema = z.object({
+    userId: z.string(),
+    productId: z.string(),
+    quantity: z.number().min(1),
+});
+export type AddToCartDTO = z.infer<typeof AddToCartSchema>;
+
+export const UpdateCartItemSchema = z.object({
+    userId: z.string(),
+    productId: z.string(),
+    quantity: z.number().min(0), // 0 para remover
+});
+export type UpdateCartItemDTO = z.infer<typeof UpdateCartItemSchema>;
